6282224643cf3daef15a18b3cb101861cd6f6e6e
Ibrahim Azhar Armar <azhar@iarmar.com>
2016-02-12 14:20:27 +0530

    Added aliases for service manager and view script for forgot-password

--- config/module.config.php
+++ config/module.config.php
@@ -4,7 +4,8 @@ return array(
     'service_manager' => array(
         'invokables' => array(
             'Oml\Zf2User\Entity\User' => 'Oml\Zf2User\Entity\User',
-            'Oml\Zf2User\Hydrator\ClassMethods' => 'Zend\Stdlib\Hydrator\ClassMethods'
+            'Oml\Zf2User\Hydrator\ClassMethods' => 'Zend\Stdlib\Hydrator\ClassMethods',
+            'AliasManager' => 'Oml\Zf2User\Service\AliasManager',
         )
     ),
     'controllers' => array(
@@ -17,6 +18,7 @@ return array(
     		'oml/account/register' => __DIR__.'/../view/zf2-user/account/register.phtml',
             'oml/account/profile' => __DIR__.'/../view/zf2-user/account/profile.phtml',
             'oml/account/change-password' => __DIR__.'/../view/zf2-user/account/change-password.phtml',
+            'oml/account/forgot-password' => __DIR__.'/../view/zf2-user/account/forgot-password.phtml',
             'oml/account/sign-in' => __DIR__.'/../view/zf2-user/account/sign-in.phtml',
     	),
     	'template_path_stack' => array(
